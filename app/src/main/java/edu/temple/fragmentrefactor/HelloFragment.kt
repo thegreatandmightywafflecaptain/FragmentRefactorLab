be971c6450aeb9f60abd75954e8905a938fecf5a
thegreatandmightywafflecaptain <sirmadametheknight@gmail.com>
2024-03-20 15:49:16 -0400

    Added functionality to fragment

--- app/src/main/java/edu/temple/fragmentrefactor/HelloFragment.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/HelloFragment.kt
@@ -5,14 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlin.random.Random
 
 class HelloFragment : Fragment() {
-  override fun onCreateView(
+    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hello, container, false)
-    }
+        return inflater.inflate(R.layout.fragment_hello, container, false).apply {
+            val displayTextView = findViewById<TextView>(R.id.displayTextView)
+            val nameEditText = findViewById<EditText>(R.id.nameEditText)
+            val changeButton = findViewById<Button>(R.id.changeButton)
 
+            changeButton.setOnClickListener {
+                val name = nameEditText.text
+
+                displayTextView.text = if (name.isNotBlank()) {
+                    "Hello, $name!"
+                } else {
+                    "Please enter your name"
+                }
+            }
+
+        }
+
+    }
 }
